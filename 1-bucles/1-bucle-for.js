7318d41c1899d7f7c761cc7ed38efcb21c57463e
Iris <irisromeropiquer@gmail.com>
2019-05-13 12:16:21 +0200

    first-exercise-done

--- 1-bucles/1-bucle-for.js
+++ 1-bucles/1-bucle-for.js
@@ -1,3 +1,7 @@
 // ej 1
 // haz un bucle que muestre por consola los números del 4 al 9 inclusive
 // Utilizad el bucle for
+
+for (var numero = 4;numero <= 9; numero++) {
+   console.log(numero);
+};

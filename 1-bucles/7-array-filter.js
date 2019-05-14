58d6daf03664ad5267ece439dbc6618a6d464784
Iris <irisromeropiquer@gmail.com>
2019-05-14 13:24:23 +0200

    exercise 7 done

--- 1-bucles/7-array-filter.js
+++ 1-bucles/7-array-filter.js
@@ -6,32 +6,38 @@
 
 
 let gente = [{
-        nombre: 'Jamiro',
-        edad: 45
-    },
-    {
-        nombre: 'Juan',
-        edad: 35
-    },
-    {
-        nombre: 'Paco',
-        edad: 34
-    },
-    {
-        nombre: 'Pepe',
-        edad: 14
-    },
-    {
-        nombre: 'Pilar',
-        edad: 24
-    },
-    {
-        nombre: 'Laura',
-        edad: 24
-    },
-    {
-        nombre: 'Jenny',
-        edad: 10
-    },
+    nombre: 'Jamiro',
+    edad: 45
+},
+{
+    nombre: 'Juan',
+    edad: 35
+},
+{
+    nombre: 'Paco',
+    edad: 34
+},
+{
+    nombre: 'Pepe',
+    edad: 14
+},
+{
+    nombre: 'Pilar',
+    edad: 24
+},
+{
+    nombre: 'Laura',
+    edad: 24
+},
+{
+    nombre: 'Jenny',
+    edad: 10
+},
 ]
 
+let personas = gente.filter(function (persona) {
+    if (persona.nombre.charAt(0)==="J" && persona.edad < 40){
+        console.log(persona.nombre)
+    }
+});
+;

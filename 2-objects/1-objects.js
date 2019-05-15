17e751b6188bd3b5f1758667716d45a23f7f6e8e
Iris <irisromeropiquer@gmail.com>
2019-05-15 13:03:16 +0200

    exercise 1-objects done

--- 2-objects/1-objects.js
+++ 2-objects/1-objects.js
@@ -6,7 +6,17 @@
 
 // Aquí tu código
 
+var ordenador = {
+    marca : "La pava",
+    tipo : "portátil",
+    perifericos : ["cosa", "touchPad", "coseta"],
+    almacenamiento: {
+        discos: ["SSD","HDD"],
+        maestro: 0,
 
+    }
+    };
+    
 
 // Hasta aquí
 
@@ -35,18 +45,21 @@ if (ordenador.perifericos[1] === 'touchPad') {
     console.log('suspendes');
 }
 // TEST 5
+// es igual que ordenador.almacenamiento.discos[1] === 'HDD'
 if (ordenador['almacenamiento']['discos'][1] === 'HDD') {
     console.log('apruebas');
 } else {
     console.log('suspendes');
 }
 // TEST 6
+// es igual que ordenador["almacenamiento"]["maestro"]===0
 if (ordenador.almacenamiento.maestro === 0) {
     console.log('apruebas');
 } else {
     console.log('suspendes');
 }
 // TEST 7
+// es igual que ordenador.almacenamiento.discos[0] === 'SSD'
 if (ordenador.almacenamiento.discos[ ordenador.almacenamiento.maestro ] === 'SSD') {
     console.log('apruebas');
     
